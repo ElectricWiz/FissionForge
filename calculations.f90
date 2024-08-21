6082213813befa17d91d8f62c4259aff1cf5f2ee
Bsc. Daniel Bazan <thecountoftouscany@ciencias.unam.mx>
2024-08-21 16:37:04 -0600

    cleanup the subroutines to calculate the paramaters for the simulation

--- calculations.f90
+++ calculations.f90
@@ -10,12 +10,17 @@ module reactor_simulation
   
   real, parameter :: NU = 2.6
   real, parameter :: ETA = 2.2
-  real, parameter :: SIGMA_F_MACRO = 0.6832 
-  real, parameter :: SIGMA_A_MACRO = 0.8052
-  real, parameter :: U235_DENSITY = 19.1 ! g/cm^3
-  real, parameter :: U235_ATOMIC_MASS = 235.0439299 ! g/mol
-  real, parameter :: AVOGADRO = 6.022E23 ! atoms/mol
-  real, parameter :: SIGMA_H20_MACRO = 0.0197
+  real, parameter :: SIGMA_F_MACRO_U235 = 0.6832 
+  real, parameter :: SIGMA_A_MACRO_U235 = 0.8052
+  real, parameter :: SIGMA_A_MACRO_H20 = 0.0197
+  real, parameter :: SIGMA_TR = 3.318
+
+  real, parameter :: SIGMA_A = SIGMA_A_MACRO_U235 + SIGMA_A_MACRO_H20
+  real, parameter :: DIFF_COEFF = 1/(3*SIGMA_TR)
+  real, parameter :: DIFF_AREA = DIFF_AREA/SIGMA_A
+
+  real, parameter :: K_INFTY = ETA*(SIGMA_A_MACRO_U235/SIGMA_A)
+  real, parameter :: BUCKLING_SQUARED = (K_INFTY - 1)/DIFF_AREA
 
 contains
 
@@ -24,9 +29,6 @@ contains
     real, allocatable, intent(out) :: flux(:), power(:)
     real, intent(out) :: k_eff
     real :: atomic_density, sigma_f, sigma_a
-    
-    call calculate_atomic_density(U235_DENSITY, U235_ATOMIC_MASS, AVOGADRO, atomic_density)
-    call calculate_macroscopic_cross_sections(atomic_density, SIGMA_F_MICRO, SIGMA_A_MICRO, sigma_f, sigma_a)
     
     call solve_diffusion(reactor_geom, NU, sigma_f, sigma_a, flux, k_eff)
     call calculate_power(flux, sigma_f, power)
@@ -41,13 +43,5 @@ contains
     atomic_density = (density * avogadro) / atomic_mass
   end subroutine calculate_atomic_density
   
-  
   ! Other subroutines for solving diffusion equation, calculating power, output etc 
-  
-  subroutine calculate_k_infty(eta, SIGMA_A_MACRO, SIGMA_H20_MACRO, k_infty)
-    real, intent(in) :: eta, SIGMA_A_MACRO, SIGMA_H20_MACRO
-    real, intent(out) :: k_infty
-
-    k_infty = eta*(SIGMA_A_MACRO/(SIGMA_H20_MACRO+SIGMA_A_MACRO))
-  end subroutine
 end module reactor_simulation
